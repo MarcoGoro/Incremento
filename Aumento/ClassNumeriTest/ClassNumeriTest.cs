260cd904b347d1ea41c57be566d1378131f7f943
Utente <Utente@UTENTE-PC>
2018-02-26 12:11:31 +0100

    Aggiunta Metodo e Test

---
+++ Aumento/ClassNumeriTest/ClassNumeriTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassNumeriTest;
+
+
+namespace ClassNumeriTest
+{
+    [TestClass]
+    public class ClassNumeriTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        [DataTestMethod]
+        [DataRow(100, 110)]
+        [DataRow(18888, 20776.8)]
+        [DataRow(3, 3)]
+        [DataRow(0, 0)]
+        [DataRow(-72, -72)]
+        public void TestIncrementaPari(double a, double ris)
+        {
+            double newnumber = ClassNumeri.IncrementaPari(a);
+            Assert.AreEqual(ris, newnumber);
+        }
+    }
+    }
+}
---
+++ Aumento/Numeri/ClassNumeri.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numeri
+{
+    public class ClassNumeri
+    {
+        public static double IncrementoNumPari(double a)
+        {
+            if (a % 2 == 0)
+                a = a * 10 / 100;
+            return a;
+
+        }
+    }
+}
